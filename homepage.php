2cdf63bd1fb8b37ecc8e23476a425eab19487f90
Aavash <aavashmdr724+github@gmail.com>
2024-03-15 23:05:55 +0545

    added login page

--- homepage.php
+++ homepage.php
@@ -1,8 +1,9 @@
 <html>
     <head>
-        <title>This is Title</title>
+        <title>HOMEPAGE</title>
     </head>
     <body>
         <h1>Welcome to homepage</h1>
+        <p>This is a test paragraph </p>
     </body>
 </html>
